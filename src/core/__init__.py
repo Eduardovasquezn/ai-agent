a9e4c8cd5a6970f7e90c77d9121b53c4091f011d
eduardo <eduardo.vasquez.nolasco@telenetgroup.be>
2025-03-27 06:41:11 +0100

    update imports

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,8 +1,8 @@
-from .llm_router import llm_router
 from .lost_item_and_shipping_info import retrieve_policy_and_shipping_info
 from .message_classification import route_message_request
 from .process_tracking import process_tracking_package_request
 from .update_user_profile import update_user_profile
+from .llm_router import llm_router
 
 __all__ = [
     "process_tracking_package_request",
